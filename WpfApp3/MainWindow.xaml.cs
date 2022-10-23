d3f96736fb817dc005acc798ed0d10f0132cd5a8
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2022-10-24 00:08:00 +0400

    last Commit

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,17 +26,120 @@ namespace WpfApp3
             InitializeComponent();
         }
 
-        private void btn1_Click(object sender, RoutedEventArgs e)
+        private void btn_Click(object sender, RoutedEventArgs e)
         {
-
             var btn = sender as Button;
             if (btn is null)
                 return;
+            int index = txtBox1.Text.Length;
+            string content = btn.Content.ToString()!;
 
-            MessageBox.Show(btn.Content.ToString());
-            
+           
 
+            switch (content)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    txtBox1.Text += content;
+                    break;
+                case "CE":
+                    txtBox1.Clear();
+                    break;
+                case "√":
+                    {
+                        if (index == 0)
+                            break;
+                        else
+                        {
+                            try
+                            {
+                                double a = Math.Sqrt(Double.Parse(txtBox1.Text));
+                                txtBox1.Text = a.ToString();
+                                break;
 
-        } 
-    }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message.ToString());
+                            }
+                        }
+                        break;
+                    }
+                case "=":
+                    try
+                    {
+                        txtBox1.Text = new DataTable().Compute(txtBox1.Text, null).ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                    break;
+                case "x²":
+                    {
+                        if (index == 0)
+                            break;
+                        try
+                        {
+                            txtBox1.Text = ((float.Parse(txtBox1.Text)) * (float.Parse(txtBox1.Text))).ToString();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message.ToString());
+                        }
+                    }
+                    break;
+                case "/":
+                    {
+                        if (index == 0)
+                            break;
+                        else if (char.IsDigit(txtBox1.Text[index - 1]) || txtBox1.Text[index - 1] == '/' && btn.Content.ToString() != "/")
+                            txtBox1.Text += content;
+                        break;
+                    }
+                case "*":
+                    {
+                        if (index == 0)
+                            break;
+                        else if (char.IsDigit(txtBox1.Text[index - 1]) || txtBox1.Text[index - 1] == '*' && btn.Content.ToString() != "*" && btn.Content.ToString() != "*")
+                            txtBox1.Text += content;
+                        break;
+                    }
+                case "-":
+                    {
+                        if (index == 0)
+                            break;
+                        else if (char.IsDigit(txtBox1.Text[index - 1]) || txtBox1.Text[index - 1] == '-' && btn.Content.ToString() != "-")
+                            txtBox1.Text += content;
+                        break;
+                    }
+                case ".":
+                    {
+                        if (index == 0)
+                            break;
+                        else if (char.IsDigit(txtBox1.Text[index - 1]) || txtBox1.Text[index - 1] == '.' && btn.Content.ToString() != ".")
+                            txtBox1.Text += content;
+                        break;
+                    }
+                case "+":
+                    {
+                        if (index == 0)
+                            break;
+                        else if (char.IsDigit(txtBox1.Text[index - 1]) || txtBox1.Text[index - 1] == '+' && btn.Content.ToString() != "+")
+                            txtBox1.Text += content;
+                        break;
+                    }
+            }
+
+        }
+
+    } 
 }
